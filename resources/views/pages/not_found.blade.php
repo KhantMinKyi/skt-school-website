210f127c26925788891de9b0fdc9644512453f71
khantminkyi <khantminkyi@gmail.com>
2025-01-22 13:49:57 +0630

    add 404 page

--- resources/views/pages/not_found.blade.php
+++ resources/views/pages/not_found.blade.php
@@ -49,7 +49,6 @@
   @import url('https://fonts.googleapis.com/css2?family=Smooch+Sans:wght@100..900&display=swap');
   </style>
 </head>
-<body>
 	<!-- START PRELOADER -->
     <div id="loader-wrapper">
         <div id="loader"></div>
@@ -57,37 +56,36 @@
         <div class="loader-section section-right"></div>
     </div>
 	<!-- END PRELOADER -->
-		<!-- START HOME -->
-		<section  id="home" class="home_bg" style="background-image: url(assets/images/banner/home.png);  background-size:cover; background-position: center center; height:100vh">	
-			{{-- <video autoplay loop muted playsinline class="video-background">
-				<source src="img/video_bg.mp4" type="video/mp4">
-				Your browser does not support the video tag.
-			</video> --}}
-			<div class="text-center home-banner-text">
-				<h3 class="pt-4">
-					Select a Campus to View!
-				</h3>
-			</div>
-			<div class="container h-100">
-				<div class="row align-items-center h-100">
-					<div class="col-lg-6 col-sm-6 col-xs-12 d-flex justify-content-center align-items-center">
-						<div style="cursor: pointer;">
-							<a href="{{route('river.home')}}">
-								<img src="{{asset('img/skt_riverside_campus.png')}}" class="img-fluid bounce-up" alt="">
-							</a>
-						</div>
-					</div>
-					<div class="col-lg-6 col-sm-6 col-xs-12 d-flex justify-content-center align-items-center">
-						<div style="cursor: pointer;">
-							<a href="{{route('river.home')}}">
-								<img src="{{asset('img/skt_city_campus.png')}}" class="img-fluid bounce-up" alt="">
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- END  HOME -->
+			<!-- START SECTION TOP -->
+            <section class="section-top">
+                <div class="container">
+                    <div class="col-lg-10 offset-lg-1 text-center">
+                        <div class="section-top-title wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.3s" data-wow-offset="0">
+                            <h1>Page Not Found</h1>
+                        </div><!-- //.HERO-TEXT -->
+                    </div><!--- END COL -->
+                </div><!--- END CONTAINER -->
+            </section>	
+            <!-- END SECTION TOP -->
+        
+            <!-- START 404 -->
+            <section class="zero_area section-padding">
+                <div class="container">
+                    <div class="row">
+                      <div class="col-lg-12 col-sm-12 col-xs-12 text-center">
+                            <div class="error_page">
+                                <img src="{{asset('assets/images/all-img/404.svg')}}" class="img-fluid" alt="404 error" />
+                                <h2>Oops! Page not found</h2>
+                                <p>Hmm. We’re having trouble finding that site. Try again later or Check your network connection.</p>
+                                <div class="home_btn">
+                                    <a href="/" class="btn_one">Back to Home</a>
+                                </div>	
+                            </div>
+                      </div><!--- END COL -->				  
+                    </div><!--- END ROW -->
+                </div><!--- END CONTAINER -->
+            </section>
+            <!-- END 404 -->
 <body>
 	{{-- {{Auth::user() ?? 'No User '}} --}}
     	
--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,33 @@ Route::get('/', function () {
 });
 Route::get('/skt-city-campus', function () {
     return view('pages.city.home');
-});
+})->name('city.home');
 Route::get('/skt-riverside-campus', function () {
     return view('pages.riverside.home');
-});
+})->name('river.home');
 
+// admin
 Route::group(['middleware' => ['auth', IsAdmin::class]], function () {
     Route::get('/administration-panel/admin/dashborad', function () {
         return view('admin.dashboard');
     });
 });
 
+// staff
 Route::group(['middleware' => ['auth', IsStaff::class]], function () {
     Route::get('/administration-panel/staff/dashborad', function () {
         return view('staff.dashboard');
     });
 });
 
+// Authentication
 Route::get('/administration-panel/login', function () {
     return view('pages.login');
 })->name('show.login');
 Route::post('/administration-panel/login', [AuthController::class, 'login'])->name('login');
 Route::post('/administration-panel/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::any('{catchall}', function () {
+    return view('pages.not_found');
+})->where('catchall', '.*');
