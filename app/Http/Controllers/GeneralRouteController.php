48c6d16e8d11388ac5ee52793ae6161761fc79a5
khantminkyi <khantminkyi@gmail.com>
2025-05-02 15:56:10 +0630

    fix event and post show in home error

--- app/Http/Controllers/GeneralRouteController.php
+++ app/Http/Controllers/GeneralRouteController.php
@@ -21,10 +21,10 @@ class GeneralRouteController extends Controller
     {
         $branch = Branch::with([
             'posts' => function ($query) {
-                $query->with('category')->where('post_is_show_front', 1);
+                $query->with('category')->where('post_is_show_front', 1)->where('post_is_active', 1);
             },
             'events' => function ($query) {
-                $query->with('category')->where('event_is_show_front', 1);
+                $query->with('category')->where('event_is_show_front', 1)->where('event_is_active', 1);
             },
             'principal_message',
             'history',
@@ -45,10 +45,10 @@ class GeneralRouteController extends Controller
     {
         $branch = Branch::with([
             'posts' => function ($query) {
-                $query->with('category')->where('post_is_show_front', 1);
+                $query->with('category')->where('post_is_show_front', 1)->where('post_is_active', 1);
             },
             'events' => function ($query) {
-                $query->with('category')->where('event_is_show_front', 1);
+                $query->with('category')->where('event_is_show_front', 1)->where('event_is_active', 1);
             },
             'principal_message',
             'history',
