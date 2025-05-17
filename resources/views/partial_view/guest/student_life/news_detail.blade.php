83d2e93a4390973b1487c1f44eac3af590e3769b
khantminkyi <khantminkyi@gmail.com>
2025-05-17 11:20:21 +0630

    remove banner in event and post

--- resources/views/partial_view/guest/student_life/news_detail.blade.php
+++ resources/views/partial_view/guest/student_life/news_detail.blade.php
@@ -4,31 +4,26 @@
     <link rel="stylesheet" href="{{ asset('guests/css/news_detail.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/slider.css') }}" />
     <!-- START POST -->
-    <div id="kenburns_061"
+    {{-- <div id="kenburns_061"
         class="carousel max-h-screen slide ps_indicators_txt_icon ps_control_txt_icon data-bs-target kbrns_zoomInOut thumb_scroll_x swipe_x ps_easeOutQuart relative w-full overflow-hidden"
         data-ride="carousel" data-pause="hover" data-interval="10000" data-duration="2000">
         <!-- Wrapper For Slides -->
-        <div class="carousel-inner flex transition-transform duration-1000 ease-in-out mt-[89px] sm:mt-0"
+        <div class="carousel-inner flex transition-transform duration-1000 ease-in-out "
             id="carouselInner">
             <!-- First Slide -->
             <div class="carousel-item active w-full flex-shrink-0">
                 <img src="{{ asset($post->post_banner) }}" alt="slider-image" class="w-full h-auto object-cover" />
-                {{-- <div class=" absolute inset-0 flex flex-col items-center justify-center p-8 text-white"
-                data-animation="animated fadeInRight">
-                <h2 class=" md:text-4xl font-semibold text-teal-500 uppercase">Alumni</h2>
-            </div> --}}
                 <div class="absolute inset-0 mx-auto flex flex-col justify-center items-center   fade-in-out will_hide_div">
                     <h1 id="fading-text" class=" text-2xl md:text-7xl text-center  ml-10 mt-20 sm:mt-0">
                         <span class="text-emerald-400">{{ $branch->branch_name }}</span>
                         <br>
-                        {{-- <div class="text-white mt-4 hidden sm:block">{{ $post->post_title }} </div> --}}
                     </h1>
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!-- START BLOG -->
-    <section class="blog-page section-padding py-12">
+    <section class="blog-page section-padding py-12 mt-[89px] ">
         <div class="container mx-auto px-4">
             <div class="row flex flex-wrap">
                 <div class="col-lg-7 col-sm-12 col-xs-12 w-full lg:w-7/12">
