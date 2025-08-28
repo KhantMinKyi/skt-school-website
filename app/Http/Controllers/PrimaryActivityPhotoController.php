c87e0bdcaba614b97353bf867b47c28209bf34a8
khantminkyi <khantminkyi@gmail.com>
2025-08-28 11:01:11 +0630

    create update and delete function in Activity Photos

--- app/Http/Controllers/PrimaryActivityPhotoController.php
+++ app/Http/Controllers/PrimaryActivityPhotoController.php
@@ -85,7 +85,14 @@ class PrimaryActivityPhotoController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $primary_activity = PrimaryActivityPhoto::find($id);
+        $branches = Branch::all();
+        $user = Auth::user();
+        if ($user->is_admin == 1) {
+            return view('partial_view.admin.primary.primary_activity_photo_edit', compact('primary_activity', 'branches'));
+        } else {
+            dd($primary_activity);
+        }
     }
 
     /**
@@ -94,6 +101,38 @@ class PrimaryActivityPhotoController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $data = $request->validate([
+            'primary_activity_file' => 'nullable|file',
+            'primary_activity_title' => 'required',
+            'primary_activity_branch_id' => 'required',
+        ]);
+        // dd($data);
+        $primary_activity = PrimaryActivityPhoto::find($id);
+        $branch = Branch::find($data['primary_activity_branch_id']);
+        $user_id = Auth::user()->id;
+        if (!$branch) {
+            return to_route('admin-primary-newsletters.index')->with('error', 'No NewsLetter Found ! Please Try Again');
+        }
+        if (isset($data['primary_activity_file'])) {
+            if (File::exists(public_path($primary_activity->primary_activity_file))) {
+                File::delete(public_path($primary_activity->primary_activity_file));
+            }
+            $filePath = "pdf/primary_activity_data/" . $branch->branch_short_name;
+            if (!File::exists($filePath)) {
+                $result = File::makeDirectory($filePath, 0755, true);
+            }
+
+            $photo = $data['primary_activity_file'];
+            $extension = $photo->getClientOriginalExtension();
+            $imageUid = uniqid('', true);
+            $photoName = $filePath . "/primary_activity_file_" . $imageUid . "." . $extension;
+
+            $photo->move($filePath, "/primary_activity_file_" . $imageUid . "." . $extension);
+            $data['primary_activity_file'] = "/" . $photoName;
+        }
+        $data['primary_activity_updated_user_id'] = $user_id;
+        $primary_activity->update($data);
+        return to_route('admin-primary-newsletters.index')->with('success', 'Activity Photo Updated Successfully');
     }
 
     /**
@@ -101,6 +140,14 @@ class PrimaryActivityPhotoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $primary_activity = PrimaryActivityPhoto::find($id);
+        if (!$primary_activity) {
+            return to_route('admin-primary-newsletters.index')->with('error', 'Activity Photo not Found');
+        }
+        if (File::exists(public_path($primary_activity->primary_activity_file))) {
+            File::delete(public_path($primary_activity->primary_activity_file));
+        }
+        $primary_activity->delete();
+        return to_route('admin-primary-newsletters.index')->with('success', 'Deleted Successfully');
     }
 }
